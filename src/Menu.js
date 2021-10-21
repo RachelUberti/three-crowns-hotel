1bb4f7c4580204b5b8337ce88ec5a96b9afe048c
Rachel Uberti <rachel.uberti@gmail.com>
2021-10-21 15:32:16 +1100

    copy updates

--- src/Menu.js
+++ src/Menu.js
@@ -1,5 +1,5 @@
 import logo from './images/logo.svg';
-import './Menu';
+import './App';
 import './scss/main.scss';
 import './scss/components/_navbar.scss';
 import './scss/components/_about.scss';
@@ -10,27 +10,21 @@ import './scss/components/_footer.scss';
 import './scss/components/_hero-image.scss';
 import './scss/components/_menu-page-body.scss';
 import './scss/components/_menu-page-navbar.scss';
-import burgerImage from './images/burger.png';
-import burgerImageCropped from './images/burger-cropped.png';
-import heroImage from './images/hero-image.png';
-import heroImageCropped from './images/hero-image-cropped.png';
-import steakImage from './images/steak.png';
-import steakImageCropped from './images/steak-cropped.png';
-import spritzImage from './images/spritz.png';
-import spritzImageCropped from './images/spritz-cropped.png';
-import { slide as Menu } from 'react-burger-menu';
+import chickenWingsImage from './images/chicken-wings.png';
+import chickenWingsImageCropped from './images/chicken-wings-cropped.png';
+import cocktailsImage from './images/cocktails.png';
+import cocktailsImageCropped from './images/cocktails-cropped.png';
 
-function App() {
+function Menu() {
   return (
-    <div className="App">
+    <div className="Menu">
       <header>
         {/* <!-- Nav bar  --> */}
-        <Menu className="menu"><a href="#book">test</a></Menu>
-        <nav class="navbar">
+        <nav id="menuNavbar" class="navbar">
           <ul class="navbar__list">
             <div class="burgerburger">&#9776;</div>
             <li>
-              <a href="Menu.js"><button class="navbar__button-menu">Menus</button></a>
+              <a href="App.js"><button class="navbar__button-menu">Home</button></a>
             </li>
             <li>
               <a class="navbar__logo" href="/">
@@ -51,7 +45,7 @@ function App() {
             <div class="overlay-content">
               <a href="#book"><button class="overlay__button-book">Book a table</button>
               </a>
-              <a href="menu.html"><button class="overlay__button-menu">Menus</button></a>
+              <a href="App.js"><button class="overlay__button-menu">Home</button></a>
               <div class="overlay__socials">
                 <a href="https://www.facebook.com/threecrownshotel"
                   class="overlay__socials-button fa fa-facebook fa-2x">facebook</a>
@@ -61,27 +55,18 @@ function App() {
             </div>
           </div>
         </nav>
-
-        {/* <!-- Hero image --> */}
-        <div>
-          <picture>
-            <source media="(max-width:400px)" srcset={heroImageCropped} />
-            <source srcset={heroImage} />
-            <img class="hero-image" src={heroImage} alt="People eating and drinking at a table" />
-          </picture>
-        </div>
       </header>
 
       {/* <!-- About section --> */}
       <section>
-        <div class="container-fluid px-0">
+        <div id="menuFirstSection" class="container-fluid px-0">
           <div class="row mx-0">
             <div class="about">
               <div class="about__text">
                 <h1 class="about__heading">Great food, booze &amp; people</h1>
                 <hr class="about__line" />
                 <p class="about__para">
-                  A no-frills West Melbourne pub, with a focus on good food, beers, service and connection with our community.
+                  Located on the corner of Victoria and King Street West Melbourne, the Three Crowns Hotel is a family owned and operated pub that prides itself on offering quality food and booze. Whether you’ve got a hankering for some Southern American classics, lip-smacking burgers, or traditional pub grub, we’ve got you sorted.
                 </p>
               </div>
             </div>
@@ -92,45 +77,22 @@ function App() {
       {/* <!-- Two Columns Section --> */}
       <section>
         <div class="container-fluid px-0">
-          <div class="row no-gutters">
-            <div class="block col-lg-6">
-              <div class="block__text">
-                <h1 class="block__heading">Location</h1>
-                <hr class="block__line" />
-                <p class="block__para">
-                  365 Victoria St, West Melbourne VIC 3003
-                </p>
-              </div>
-            </div>
-            <div class="col-lg-6">
-              <picture>
-                <source media="(max-width:400px)" srcset={spritzImageCropped} />
-                <source srcset={spritzImage} />
-                <img class="block-image" src={spritzImage} alt="Aperol Spritz Cocktail" />
-              </picture>
-            </div>
-          </div>
-        </div>
-      </section>
-
-      <section>
-        <div class="container-fluid px-0">
           <div class="row no-gutters flex-column-reverse flex-lg-row">
             <div class="col-lg-6">
               <picture>
-                <source media="(max-width:400px)" srcset={steakImageCropped} />
-                <source srcset={steakImage} />
-                <img class="block-image" src={steakImage} alt="Red wine jus being poured on a plated steak" />
+                <source media="(max-width:400px)" srcset={chickenWingsImageCropped} />
+                <source srcset={chickenWingsImage} />
+                <img class="block-image" src={chickenWingsImage} alt="Chicken wings in a basket" />
               </picture>
             </div>
-            <div class="block col-lg-6">
-              <div class="block__text">
-                <h1 class="block__heading">Open hours</h1>
-                <hr class="block__line" />
-                <p class="block__para">Monday - Tuesday 4pm - 10pm</p>
-                <p class="block__para">Wednesday 12pm - 10pm</p>
-                <p class="block__para">Thursday - Saturday 12pm - Late</p>
-                <p class="block__para">Sunday 12pm - 11pm</p>
+            <div class="menu col-lg-6">
+              <div class="menu__text">
+                <p class="menu__para">
+                  Pub classics meet American favourites.
+                </p>
+                <button class="menu__button"
+                  onclick="window.location.href='https:/drive.google.com/file/d/15f-QJsmNq4uZlGe4fe39t4LrntfrgPHT/view?usp=sharing'">View
+                  food menu</button>
               </div>
             </div>
           </div>
@@ -141,22 +103,18 @@ function App() {
         <div class="container-fluid px-0">
           <div class="row no-gutters">
             <div class="block col-lg-6">
-              <div class="block__text">
-                <h1 class="block__heading">Contact</h1>
-                <hr class="block__line" />
-                <p class="block__para">info@threecrownshotel.com.au</p>
-                <div class="block__phone">
-                  <span itemprop="telephone">
-                    <a class="block__phone-link" href="tel:03-93265033">03 9326 5033</a>
-                  </span>
-                </div>
+              <div class="menu__text">
+                <p class="menu__para">
+                  Beers, cocktails & wines.
+                </p>
+                <button class="menu__button">View drinks menu</button>
               </div>
             </div>
             <div class="col-lg-6">
               <picture>
-                <source media="(max-width:400px)" srcset={burgerImageCropped} />
-                <source srcset={burgerImage} />
-                <img class="block-image" src={burgerImage} alt="Burger in a basket with fries" />
+                <source media="(max-width:400px)" srcset={cocktailsImageCropped} />
+                <source srcset={cocktailsImage} />
+                <img class="block-image" src={cocktailsImage} alt="Cocktails in different shaped glasses" />
               </picture>
             </div>
           </div>
@@ -181,10 +139,8 @@ function App() {
                   </span>
                 </div>
                 <div class="footer__socials">
-                  <a href="https://www.facebook.com/threecrownshotel"
-                    class="footer__socials-button fa fa-facebook fa-2x">facebook</a>
-                  <a href="https://www.instagram.com/threecrownshotel/?hl=en"
-                    class="footer__socials-button fa fa-instagram fa-2x">instagram</a>
+                  <a href="#" class="footer__socials-button fa fa-facebook fa-2x">facebook</a>
+                  <a href="#" class="footer__socials-button fa fa-instagram fa-2x">Instagram</a>
                 </div>
                 <p class="footer__credit">UI design by Dominique Pooley and engineering by Rachel Uberti
                 </p>
@@ -197,4 +153,4 @@ function App() {
   );
 }
 
-export default App;
+export default Menu;
