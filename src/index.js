3f0f9dd288abc6edaefcd181a8177166564ddd4a
Rachel Uberti <rachel.uberti@gmail.com>
2021-10-26 08:12:18 +1100

    font family import

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import WebFont from 'webfontloader';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -11,7 +12,11 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-
+WebFont.load({
+  google: {
+    families: ['Montserrat Web:400,700', 'Roboto', 'sans-serif']
+  }
+});
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
