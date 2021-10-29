d6b1c0d907773143105a7d979d7c4b88eab188ea
Rachel Uberti <rachel.uberti@gmail.com>
2021-10-29 15:05:55 +1100

    slide menu styling

--- src/SlideMenu.js
+++ src/SlideMenu.js
@@ -1,5 +1,6 @@
 import { slide as Menu } from 'react-burger-menu';
 import { BOOKINGLINK } from './constants';
+import { SocialIcon } from 'react-social-icons';
 import './scss/components/_navbar.scss';
 import './scss/components/_slide-menu.scss';
 
@@ -13,6 +14,10 @@ function SlideMenu() {
       <a href="/menuspage">
         <button className='slide-menu__Menus-button'>Menus</button>
       </a>
+      <div className="footer__socials">
+        <SocialIcon className="footer__socials-button" url="https://www.facebook.com/threecrownshotel" network="facebook" bgColor="#000000" fgColor="#FFFFFF" color="currentColor" style={{ height: 65, width: 65 }} />
+        <SocialIcon className="footer__socials-button" url="https://www.instagram.com/threecrownshotel/?hl=en" network="instagram" bgColor="#000000" fgColor="#FFFFFF" style={{ height: 65, width: 65 }} />
+      </div>
     </Menu>
   );
 }
