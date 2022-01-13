bfcb21c3f3d4ea4e67d92dfbd80e002ae11f2022
Rachel Uberti <rachel.uberti@gmail.com>
2022-01-14 10:24:26 +1100

    updated open table link

--- src/constants.js
+++ src/constants.js
@@ -1,1 +1,1 @@
-export const BOOKINGLINK = 'https://www.bookarestaurant.com/widget/BookingWidgetView/3466/Three-Crowns-Hotel/3/en-AU';
+export const BOOKINGLINK = 'https://www.opentable.com.au/r/three-crowns-hotel-reservations-west-melbourne?restref=22346&lang=en-AU&ot_source=Restaurant%20website';
